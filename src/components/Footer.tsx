b4573aeecb7b2b727bc048db23510fab5c501bf9
Ediomojose.Dev <goodnewsediomojoshua@yahoo.com>
2023-07-30 22:13:16 +0100

    Fotter componnets updated

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -8,10 +8,10 @@ import InstagramIcon from "../assets/images/icon-instagram.svg";
 const Footer = () => {
   return (
     <>
-      <footer>
-        <div>
-          <img src={Logo} alt="insure logo" />
-          <ul>
+      <footer className="grid  grid-cols-1 px-10 py-10 md:px-20 md:py-20 lg:px-28 lg:py-24">
+        <div className="grid grid-rows-2 gap-y-5 my-5 md:grid-cols-2 md:gap-y-0">
+          <img src={Logo} alt="insure logo" className="mx-auto"/>
+          <ul className="flex justify-center  gap-x-4 md:justify-end">
             <li>
               <a href="http://" target="_blank" rel="noopener noreferrer">
                 <img src={FacebookIcon} alt="insure logo" />
@@ -34,25 +34,26 @@ const Footer = () => {
             </li>
           </ul>
         </div>
-        <div>
+        <hr />
+        <div className=" grid grid-cols-1 mx-auto justify-center mt-8 text-center md:grid-cols-4 md:text-start" >
           <ul>
-            <li>OUR COMPANY</li>
-            <li>
+            <li className="pb-5">OUR COMPANY</li>
+            <li className="pb-2">
               <a href="http://" target="_blank" rel="noopener noreferrer">
                 HOW WE WORK
               </a>
             </li>
-            <li>
+            <li className="pb-2">
               <a href="http://" target="_blank" rel="noopener noreferrer">
                 WHY INSURE?
               </a>
             </li>
-            <li>
+            <li className="pb-2">
               <a href="http://" target="_blank" rel="noopener noreferrer">
                 VIEW PLANS
               </a>
             </li>
-            <li>
+            <li className="pb-2">
               <a href="http://" target="_blank" rel="noopener noreferrer">
                 REVIEWS
               </a>
@@ -60,23 +61,23 @@ const Footer = () => {
           </ul>
 
           <ul>
-          <li>HELP ME</li>
-            <li>
+          <li className="pb-5">HELP ME</li>
+            <li className="pb-2">
               <a href="http://" target="_blank" rel="noopener noreferrer">
                 FAQ
               </a>
             </li>
-            <li>
+            <li className="pb-2">
               <a href="http://" target="_blank" rel="noopener noreferrer">
                 TERMS OF USE
               </a>
             </li>
-            <li>
+            <li className="pb-2">
               <a href="http://" target="_blank" rel="noopener noreferrer">
                 PRIVACY POLICY
               </a>
             </li>
-            <li>
+            <li className="pb-2">
               <a href="http://" target="_blank" rel="noopener noreferrer">
                 COOKIES
               </a>
@@ -84,18 +85,13 @@ const Footer = () => {
           </ul>
 
           <ul>
-          <li>CONTACT</li>
-            <li>
+          <li className="pb-5">CONTACT</li>
+            <li className="pb-2">
               <a href="http://" target="_blank" rel="noopener noreferrer">
                 SALES
               </a>
             </li>
-            <li>
-              <a href="http://" target="_blank" rel="noopener noreferrer">
-                SUPPORT
-              </a>
-            </li>
-            <li>
+            <li className="pb-2">
               <a href="http://" target="_blank" rel="noopener noreferrer">
                 LIVE CHAT
               </a>
@@ -103,18 +99,18 @@ const Footer = () => {
           </ul>
 
           <ul>
-          <li>OTHERS</li>
-            <li>
+          <li className="pb-5">OTHERS</li>
+            <li className="pb-2">
               <a href="http://" target="_blank" rel="noopener noreferrer">
                 CAREERS
               </a>
             </li>
-            <li>
+            <li className="pb-2">
               <a href="http://" target="_blank" rel="noopener noreferrer">
                 PRESS
               </a>
             </li>
-            <li>
+            <li className="pb-2">
               <a href="http://" target="_blank" rel="noopener noreferrer">
                 LICENSES
               </a>
