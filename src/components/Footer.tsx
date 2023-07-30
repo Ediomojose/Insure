cf8eaee8e30aef9265fbdea2105bf6116c2923ce
Ediomojose.Dev <goodnewsediomojoshua@yahoo.com>
2023-07-30 11:29:25 +0100

    Imported icons to the footer component

---
+++ src/components/Footer.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import Logo from '../assets/images/logo.svg';
+import FacebookIcon from '../assets/images/icon-facebook.svg';
+import TwitterIcon from '../assets/images/icon-twitter.svg';
+import PinterestIcon from '../assets/images/icon-pinterest.svg';
+import InstagramIcon from '../assets/images/icon-instagram.svg';
+
+const Footer = () => {
+  return (
+    <>
+    <footer>
+        <div>
+
+        </div>
+    </footer>
+    </>
+  )
+}
+
+export default Footer
